da9e39b09ab872232150ac7bdc4538915be50e1f
dvera123 <dinamovera@gmail.com>
2014-06-23 13:09:18 -0600

    architecture update

---
+++ admin/player_view/player_view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Players</h1>
+        <?php
+            $con = new mysqli('localhost', 'root', 'root', 'soccer_play_by_play');
+ 
+            $result = mysqli_query($con, 
+                "CALL sp_get_players") or die("Query fail: " . mysqli_error());
+
+             //loop the result set
+             while ($row = mysqli_fetch_array($result)){   
+                 echo $row['name']; 
+             } 
+        ?>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -10,18 +10,6 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <h1>Players</h1>
-        <?php
-            $con = new mysqli('localhost', 'root', 'root', 'soccer_play_by_play');
- 
-            $result = mysqli_query($con, 
-                "CALL sp_get_players") or die("Query fail: " . mysqli_error());
-
-             //loop the result set
-             while ($row = mysqli_fetch_array($result)){   
-                 echo $row['name']; 
-             }
-            
-        ?>
+        
     </body>
 </html>
