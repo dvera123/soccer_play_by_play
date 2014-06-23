8dcd6e393bfdc9e393de5a19d51bff1722e6f338
dvera123 <dinamovera@gmail.com>
2014-06-23 13:26:52 -0600

    architecture update

--- admin/player_view/player_view.php
+++ admin/player_view/player_view.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+require "../util/Connection.php";
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -12,15 +10,15 @@ and open the template in the editor.
     <body>
         <h1>Players</h1>
         <?php
-            $con = new mysqli('localhost', 'root', 'root', 'soccer_play_by_play');
- 
+            $con = getConnection();
+            
             $result = mysqli_query($con, 
                 "CALL sp_get_players") or die("Query fail: " . mysqli_error());
 
              //loop the result set
              while ($row = mysqli_fetch_array($result)){   
                  echo $row['name']; 
-             } 
+             }
         ?>
     </body>
 </html>
---
+++ admin/util/Connection.php
@@ -0,0 +1,10 @@
+<?php
+const  SERVER = "localhost";
+const USER = "root";
+const PASS = "root";
+const DB = "soccer_play_by_play";
+
+function getConnection(){
+    return  new mysqli(SERVER, USER, PASS, DB);
+}
+
